a9cedf89d1d8f16a613274f10be71712554f8a04
mlamberty <mattlamberty@outlook.com>
2021-01-23 13:12:37 -0600

    Static files

--- Python_Flask_Web/Python_Flask_Web/views.py
+++ Python_Flask_Web/Python_Flask_Web/views.py
@@ -10,11 +10,13 @@ from Python_Flask_Web import app
 @app.route('/home')
 def home():
     """Renders the home page."""
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
     return render_template(
         'index.html',
         title='Home Page',
-        year=datetime.now().year,
-    )
+        )
 
 @app.route('/contact')
 def contact():
